f7e9dbf40b048698ee8f3c4c1e890d21d87c506b
Unknown <ftrestour@gmail.com>
2018-05-04 11:44:22 +0200

    Comments

--- getData.js
+++ getData.js
@@ -5,17 +5,22 @@ const urlBase = 'http://catb.org/jargon/html/'
 const url = 'http://catb.org/jargon/html/go01.html'
 const slashes = url.split('/').length
 
+// Gets all of the entries registered in the lexicon
 const getEntries = async url => {
   try {
+    // Extracting the links
     const dom = await JSDOM.fromURL(url)
     const document = dom.window.document
     const links = document.querySelectorAll('a')
 
+    // Cleaning data
     const entries = Object.values(links).map(link => ({
       name: link.textContent,
       url: link.href
     }))
 
+    // Removing pages which are not a definition
+    // Comparing the page's depth in the website's tree allows to do that
     const filteredEntries = entries.filter(
       entry => entry.url && entry.url.split('/').length !== slashes
     )
@@ -26,21 +31,27 @@ const getEntries = async url => {
   }
 }
 
+// Returns a random entry from the given entries
 const randomEntry = entries =>
   entries[Math.floor(Math.random() * Math.floor(entries.length))]
 
+// Getting the description and related pages for a specified entry from the lexicon
 const getEntryDetails = async entry => {
   try {
     const dom = await JSDOM.fromURL(entry.url)
     const document = dom.window.document
 
+    // Getting the description
     const descriptionElement = document.querySelector('p')
 
+    // The links are filtered to make sur they are also a lexicon entry
     const linksWithDuplicates = Object.values(document.querySelectorAll('a'))
       .map(link => link.href)
       .filter(url => url && url.split('/').length !== slashes)
+    // Some links appear twice in some articles : de-duplicating
     const links = Array.from(new Set(linksWithDuplicates))
 
+    // Removing unnecessary \n in descriptions
     const description = descriptionElement.textContent.replace(
       /(\n+\s*)+/g,
       ' '
