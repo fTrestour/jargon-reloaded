00d6d4b66aaf8337ac430326ccc5a3944e4db79d
Unknown <ftrestour@gmail.com>
2018-05-07 15:01:25 +0200

    Flat file

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import * as d3 from 'd3'
 
 import entries from './entries.json'
-import { randomEntry, getRelatedEntry } from './processData.js'
+
+// Returns a random entry from the given entries
+export const randomEntry = entries =>
+  entries[Math.floor(Math.random() * Math.floor(entries.length))]
+
+// Fetches the right entry in the lexicon for an url
+export const getRelatedEntry = entries => url =>
+  entries.find(entry => entry.url === url)
 
 const createLink = (source, target) => ({
   source,
--- processData.js
+++
@@ -1,7 +0,0 @@
-// Returns a random entry from the given entries
-export const randomEntry = entries =>
-  entries[Math.floor(Math.random() * Math.floor(entries.length))]
-
-// Fetches the right entry in the lexicon for an url
-export const getRelatedEntry = entries => url =>
-  entries.find(entry => entry.url === url)
