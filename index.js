44e0e47c038408d879386d229fd8b22e705c50a7
Unknown <ftrestour@gmail.com>
2018-05-07 11:41:14 +0200

    Drag and drop

--- index.js
+++ index.js
@@ -50,6 +50,25 @@ var simulation = d3
   .force('charge', d3.forceManyBody().strength(-120))
   .force('center', d3.forceCenter(width / 2, height / 2))
 
+const dragDrop = d3
+  .drag()
+  .on('start', node => {
+    node.fx = node.x
+    node.fy = node.y
+  })
+  .on('drag', node => {
+    simulation.alphaTarget(0.7).restart()
+    node.fx = d3.event.x
+    node.fy = d3.event.y
+  })
+  .on('end', node => {
+    if (!d3.event.active) {
+      simulation.alphaTarget(0)
+    }
+    node.fx = null
+    node.fy = null
+  })
+
 const getNodeColor = node => 'gray'
 
 // Display nodes
@@ -81,10 +100,11 @@ const linkElements = svg
   .enter()
   .append('line')
   .attr('stroke-width', 1)
-  .attr('stroke', '#B12100')
+  .attr('stroke', 'rgba(#000,.4)')
 
 // Tick simulation
 simulation.nodes(graph.nodes).on('tick', () => {
+  nodeElements.call(dragDrop)
   nodeElements.attr('cx', node => node.x).attr('cy', node => node.y)
   textElements.attr('x', node => node.x).attr('y', node => node.y)
   linkElements
