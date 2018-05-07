83f3eb192868d6477c22cb021b820744b96b694a
Unknown <ftrestour@gmail.com>
2018-05-07 17:03:59 +0200

    First step refacto

---
+++ graph.js
@@ -0,0 +1,238 @@
+import * as d3 from 'd3'
+
+class Graph {
+  constructor(opts) {
+    // load in arguments from config object
+    this.data = opts.data
+    this.element = opts.element
+    this.nodeIds = opts.nodeIds
+    this.mainColor = opts.mainColor
+    this.secondaryColor = opts.secondaryColor
+
+    // create the chart
+    this.generate()
+  }
+
+  setData(newData) {
+    this.data = newData
+
+    // full redraw needed
+    this.draw()
+  }
+
+  generate() {
+    // define width, height and margin
+    this.width = this.element.offsetWidth
+    this.height = window.height
+
+    // set up parent element and SVG
+    this.element.innerHTML = ''
+    const svg = d3.select(this.element).append('svg')
+    svg.attr('width', this.width)
+    svg.attr('height', this.height)
+
+    // set up groups
+    this.linkGroup = svg.append('g').attr('class', 'links')
+    this.nodeGroup = svg.append('g').attr('class', 'nodes')
+    this.textGroup = svg.append('g').attr('class', 'texts')
+
+    this.linkElements = null
+    this.nodeElements = null
+    this.textElements = null
+
+    // create the other stuff
+    this.addInitialGraph()
+    this.addLinkForce()
+    this.createSimulation()
+    this.addDragDrop()
+
+    // launch drawing
+    this.update()
+  }
+
+  addInitialGraph() {
+    const firstNode = this.data[
+      Math.floor(Math.random() * Math.floor(this.data.length))
+    ]
+
+    this.graph = {
+      nodes: [firstNode],
+      links: []
+    }
+  }
+  addLinkForce() {
+    this.linkForce = d3
+      .forceLink()
+      .id(link => link.url)
+      .strength(link => 1)
+  }
+  createSimulation() {
+    this.simulation = d3
+      .forceSimulation()
+      .force('link', this.linkForce)
+      .force('charge', d3.forceManyBody().strength(-120))
+      .force('center', d3.forceCenter(this.width / 2, this.height / 2))
+  }
+  addDragDrop() {
+    this.dragDrop = d3
+      .drag()
+      .on('start', node => {
+        node.fx = node.x
+        node.fy = node.y
+      })
+      .on('drag', node => {
+        this.simulation.alphaTarget(0.7).restart()
+        node.fx = d3.event.x
+        node.fy = d3.event.y
+      })
+      .on('end', node => {
+        if (!d3.event.active) {
+          this.simulation.alphaTarget(0)
+        }
+        node.fx = null
+        node.fy = null
+      })
+  }
+
+  selectNode(selectedNode) {
+    const neighbors = this.getNeighbors(selectedNode)
+
+    this.update(selectedNode)
+
+    this.nodeElements.attr('fill', node => this.getNodeColor(node, neighbors))
+    this.textElements.attr('fill', node => this.getTextColor(node, neighbors))
+    this.linkElements.attr('stroke', link =>
+      this.getLinkColor(selectedNode, link)
+    )
+  }
+
+  getNeighbors(node) {
+    return node ? [node.url, ...node.links] : []
+  }
+  isNeighborNode(node, neighbors) {
+    return neighbors && neighbors.indexOf(node.url) !== -1
+  }
+  isNeighborLink(node, link) {
+    return (
+      node && (link.target.url === node.url || link.source.url === node.url)
+    )
+  }
+  getNodeColor(node, neighbors) {
+    return this.isNeighborNode(node, neighbors)
+      ? this.mainColor
+      : this.secondaryColor
+  }
+  getTextColor(node, neighbors) {
+    return this.isNeighborNode(node, neighbors)
+      ? this.mainColor
+      : this.secondaryColor
+  }
+  getLinkColor(node, link) {
+    return this.isNeighborLink(node, link)
+      ? this.mainColor
+      : this.secondaryColor
+  }
+
+  update(selectedNode) {
+    this.updateData(selectedNode)
+    this.updateLinks()
+    this.updateNodes()
+    this.updateTexts()
+    this.updateSimulation()
+  }
+
+  updateData(selectedNode) {
+    selectedNode = selectedNode || this.graph.nodes[0]
+
+    const oldNodes = this.graph.nodes
+    const oldLinks = this.graph.links
+
+    const newNodes = selectedNode.links
+      .map(url => this.data.find(entry => entry.url === url))
+      .filter(node => oldNodes.indexOf(node) === -1)
+    const newLinks = selectedNode.links
+      .filter(
+        url =>
+          oldLinks.indexOf(this.createLink(selectedNode.url, url)) === -1 &&
+          oldLinks.indexOf(this.createLink(url, selectedNode.url)) === -1
+      )
+      .map(url => this.createLink(selectedNode.url, url))
+
+    newNodes.forEach(node => this.graph.nodes.push(node))
+    newLinks.forEach(link => this.graph.links.push(link))
+  }
+  updateLinks() {
+    this.linkElements = this.linkGroup
+      .selectAll('line')
+      .data(this.graph.links, link => link.target.url + link.source.url)
+
+    this.linkElements.exit().remove()
+
+    const linkEnter = this.linkElements
+      .enter()
+      .append('line')
+      .attr('stroke-width', 1)
+      .attr('stroke', link => this.getLinkColor(null, link))
+
+    this.linkElements = linkEnter.merge(this.linkElements)
+  }
+  updateNodes() {
+    this.nodeElements = this.nodeGroup
+      .selectAll('circle')
+      .data(this.graph.nodes, node => node.url)
+
+    this.nodeElements.exit().remove()
+
+    const nodeEnter = this.nodeElements
+      .enter()
+      .append('circle')
+      .attr('r', 10)
+      .attr('fill', node => this.getTextColor(node))
+      .call(this.dragDrop)
+      // we link the selectNode method here
+      // to update the graph on every click
+      .on('click', this.selectNode)
+
+    this.nodeElements = nodeEnter.merge(this.nodeElements)
+  }
+  updateTexts() {
+    this.textElements = this.textGroup
+      .selectAll('text')
+      .data(this.graph.nodes, node => node.url)
+
+    this.textElements.exit().remove()
+
+    const textEnter = this.textElements
+      .enter()
+      .append('text')
+      .text(node => node.name)
+      .attr('font-size', 15)
+      .attr('dx', 15)
+      .attr('dy', 4)
+
+    this.textElements = textEnter.merge(this.textElements)
+  }
+  updateSimulation() {
+    this.simulation.nodes(this.graph.nodes).on('tick', () => {
+      this.nodeElements.attr('cx', node => node.x).attr('cy', node => node.y)
+      this.textElements.attr('x', node => node.x).attr('y', node => node.y)
+      this.linkElements
+        .attr('x1', link => link.source.x)
+        .attr('y1', link => link.source.y)
+        .attr('x2', link => link.target.x)
+        .attr('y2', link => link.target.y)
+    })
+
+    this.simulation.force('link').links(this.graph.links)
+    this.simulation.restart()
+  }
+
+  createLink(source, target) {
+    return {
+      source,
+      target
+    }
+  }
+}
+
+export default Graph
--- index.js
+++ index.js
@@ -1,205 +1,201 @@
 import * as d3 from 'd3'
 
 import entries from './entries.json'
+import Graph from './graph'
 
-// Returns a random entry from the given entries
-export const randomEntry = entries =>
-  entries[Math.floor(Math.random() * Math.floor(entries.length))]
-
-// Fetches the right entry in the lexicon for an url
-export const getRelatedEntry = entries => url =>
-  entries.find(entry => entry.url === url)
-
-const createLink = (source, target) => ({
-  source,
-  target
+const graph = new Graph({
+  data: entries,
+  element: document.querySelector('.chart-container'),
+  mainColor: 'gray',
+  secondaryColor: 'blue'
 })
 
-const firstEntry = randomEntry(entries)
-const graph = {
-  nodes: [firstEntry],
-  links: []
-}
-
-const updateData = selectedNode => {
-  const oldNodes = graph.nodes
-  const oldLinks = graph.links
-
-  const newNodes = selectedNode.links
-    .map(getRelatedEntry(entries))
-    .filter(node => oldNodes.indexOf(node) === -1)
-  const newLinks = selectedNode.links
-    .filter(
-      url =>
-        oldLinks.indexOf(createLink(selectedNode.url, url)) === -1 &&
-        oldLinks.indexOf(createLink(url, selectedNode.url)) === -1
-    )
-    .map(url => createLink(selectedNode.url, url))
-
-  newNodes.forEach(node => graph.nodes.push(node))
-  newLinks.forEach(link => graph.links.push(link))
-}
-
-// Set the svg
-const width = window.innerWidth
-const height = window.innerHeight
-const svg = d3
-  .select('svg')
-  .attr('width', width)
-  .attr('height', height)
-
-// Set the simulation details
-var linkForce = d3
-  .forceLink()
-  .id(link => link.url)
-  .strength(link => 1)
-
-var simulation = d3
-  .forceSimulation()
-  .force('link', linkForce)
-  .force('charge', d3.forceManyBody().strength(-120))
-  .force('center', d3.forceCenter(width / 2, height / 2))
-
-const dragDrop = d3
-  .drag()
-  .on('start', node => {
-    node.fx = node.x
-    node.fy = node.y
-  })
-  .on('drag', node => {
-    simulation.alphaTarget(0.7).restart()
-    node.fx = d3.event.x
-    node.fy = d3.event.y
-  })
-  .on('end', node => {
-    if (!d3.event.active) {
-      simulation.alphaTarget(0)
-    }
-    node.fx = null
-    node.fy = null
-  })
-
-const getNeighbors = node => (node ? [node.url, ...node.links] : [])
-const isNeighborLink = (node, link) =>
-  node && (link.target.url === node.url || link.source.url === node.url)
-
-const getNodeColor = (node, neighbors) =>
-  neighbors && neighbors.indexOf(node.url) !== -1 ? 'blue' : 'black'
-const getTextColor = (node, neighbors) =>
-  neighbors && neighbors.indexOf(node.url) !== -1 ? 'blue' : 'black'
-const getLinkColor = (node, link) =>
-  isNeighborLink(node, link) ? 'blue' : 'black'
-
-const selectNode = selectedNode => {
-  const neighbors = getNeighbors(selectedNode)
-
-  updateData(selectedNode)
-  updateSimulation()
-
-  nodeElements.attr('fill', node => getNodeColor(node, neighbors))
-  textElements.attr('fill', node => getTextColor(node, neighbors))
-  linkElements.attr('stroke', link => getLinkColor(selectedNode, link))
-}
-
-const linkGroup = svg.append('g').attr('class', 'links')
-const nodeGroup = svg.append('g').attr('class', 'nodes')
-const textGroup = svg.append('g').attr('class', 'texts')
-let linkElements, nodeElements, textElements
-
-const updateGraph = () => {
-  // links
-  linkElements = linkGroup.selectAll('line').data(graph.links, function(link) {
-    return link.target.url + link.source.url
-  })
-
-  linkElements.exit().remove()
-
-  var linkEnter = linkElements
-    .enter()
-    .append('line')
-    .attr('stroke-width', 1)
-    .attr('stroke', link => getLinkColor(null, link))
-
-  linkElements = linkEnter.merge(linkElements)
-
-  // nodes
-  nodeElements = nodeGroup
-    .selectAll('circle')
-    .data(graph.nodes, function(node) {
-      return node.id
-    })
-
-  nodeElements.exit().remove()
-
-  var nodeEnter = nodeElements
-    .enter()
-    .append('circle')
-    .attr('r', 10)
-    .attr('fill', node => getTextColor(node))
-    .call(dragDrop)
-    // we link the selectNode method here
-    // to update the graph on every click
-    .on('click', selectNode)
-
-  nodeElements = nodeEnter.merge(nodeElements)
-
-  // texts
-  textElements = textGroup.selectAll('text').data(graph.nodes, function(node) {
-    return node.url
-  })
-
-  textElements.exit().remove()
-
-  var textEnter = textElements
-    .enter()
-    .append('text')
-    .text(function(node) {
-      return node.name
-    })
-    .attr('font-size', 15)
-    .attr('dx', 15)
-    .attr('dy', 4)
-
-  textElements = textEnter.merge(textElements)
-}
-
-const updateSimulation = () => {
-  updateGraph()
-
-  simulation.nodes(graph.nodes).on('tick', () => {
-    nodeElements
-      .attr('cx', function(node) {
-        return node.x
-      })
-      .attr('cy', function(node) {
-        return node.y
-      })
-    textElements
-      .attr('x', function(node) {
-        return node.x
-      })
-      .attr('y', function(node) {
-        return node.y
-      })
-    linkElements
-      .attr('x1', function(link) {
-        return link.source.x
-      })
-      .attr('y1', function(link) {
-        return link.source.y
-      })
-      .attr('x2', function(link) {
-        return link.target.x
-      })
-      .attr('y2', function(link) {
-        return link.target.y
-      })
-  })
-
-  simulation.force('link').links(graph.links)
-  simulation.restart()
-}
-
-simulation.force('link').links(graph.links)
-
-updateSimulation()
+// const firstEntry = randomEntry(entries)
+// const graph = {
+//   nodes: [firstEntry],
+//   links: []
+// }
+// +
+//
+// const updateData = selectedNode => {
+//   const oldNodes = graph.nodes
+//   const oldLinks = graph.links
+//
+//   const newNodes = selectedNode.links
+//     .map(getRelatedEntry(entries))
+//     .filter(node => oldNodes.indexOf(node) === -1)
+//   const newLinks = selectedNode.links
+//     .filter(
+//       url =>
+//         oldLinks.indexOf(createLink(selectedNode.url, url)) === -1 &&
+//         oldLinks.indexOf(createLink(url, selectedNode.url)) === -1
+//     )
+//     .map(url => createLink(selectedNode.url, url))
+//
+//   newNodes.forEach(node => graph.nodes.push(node))
+//   newLinks.forEach(link => graph.links.push(link))
+// }
+//
+// // Set the svg
+// const width = window.innerWidth
+// const height = window.innerHeight
+// const svg = d3
+//   .select('svg')
+//   .attr('width', width)
+//   .attr('height', height)
+//
+// // Set the simulation details
+// var linkForce = d3
+//   .forceLink()
+//   .id(link => link.url)
+//   .strength(link => 1)
+//
+// var simulation = d3
+//   .forceSimulation()
+//   .force('link', linkForce)
+//   .force('charge', d3.forceManyBody().strength(-120))
+//   .force('center', d3.forceCenter(width / 2, height / 2))
+//
+// const dragDrop = d3
+//   .drag()
+//   .on('start', node => {
+//     node.fx = node.x
+//     node.fy = node.y
+//   })
+//   .on('drag', node => {
+//     simulation.alphaTarget(0.7).restart()
+//     node.fx = d3.event.x
+//     node.fy = d3.event.y
+//   })
+//   .on('end', node => {
+//     if (!d3.event.active) {
+//       simulation.alphaTarget(0)
+//     }
+//     node.fx = null
+//     node.fy = null
+//   })
+//
+// const getNeighbors = node => (node ? [node.url, ...node.links] : [])
+// const isNeighborLink = (node, link) =>
+//   node && (link.target.url === node.url || link.source.url === node.url)
+//
+// const getNodeColor = (node, neighbors) =>
+//   neighbors && neighbors.indexOf(node.url) !== -1 ? 'blue' : 'black'
+// const getTextColor = (node, neighbors) =>
+//   neighbors && neighbors.indexOf(node.url) !== -1 ? 'blue' : 'black'
+// const getLinkColor = (node, link) =>
+//   isNeighborLink(node, link) ? 'blue' : 'black'
+//
+// const selectNode = selectedNode => {
+//   const neighbors = getNeighbors(selectedNode)
+//
+//   updateData(selectedNode)
+//   updateSimulation()
+//
+//   nodeElements.attr('fill', node => getNodeColor(node, neighbors))
+//   textElements.attr('fill', node => getTextColor(node, neighbors))
+//   linkElements.attr('stroke', link => getLinkColor(selectedNode, link))
+// }
+//
+// const linkGroup = svg.append('g').attr('class', 'links')
+// const nodeGroup = svg.append('g').attr('class', 'nodes')
+// const textGroup = svg.append('g').attr('class', 'texts')
+// let linkElements, nodeElements, textElements
+//
+// const updateGraph = () => {
+//   // links
+//   linkElements = linkGroup.selectAll('line').data(graph.links, function(link) {
+//     return link.target.url + link.source.url
+//   })
+//
+//   linkElements.exit().remove()
+//
+//   var linkEnter = linkElements
+//     .enter()
+//     .append('line')
+//     .attr('stroke-width', 1)
+//     .attr('stroke', link => getLinkColor(null, link))
+//
+//   linkElements = linkEnter.merge(linkElements)
+//
+//   // nodes
+//   nodeElements = nodeGroup
+//     .selectAll('circle')
+//     .data(graph.nodes, function(node) {
+//       return node.id
+//     })
+//
+//   nodeElements.exit().remove()
+//
+//   var nodeEnter = nodeElements
+//     .enter()
+//     .append('circle')
+//     .attr('r', 10)
+//     .attr('fill', node => getTextColor(node))
+//     .call(dragDrop)
+//     // we link the selectNode method here
+//     // to update the graph on every click
+//     .on('click', selectNode)
+//
+//   nodeElements = nodeEnter.merge(nodeElements)
+//
+//   // texts
+//   textElements = textGroup.selectAll('text').data(graph.nodes, function(node) {
+//     return node.url
+//   })
+//
+//   textElements.exit().remove()
+//
+//   var textEnter = textElements
+//     .enter()
+//     .append('text')
+//     .text(function(node) {
+//       return node.name
+//     })
+//     .attr('font-size', 15)
+//     .attr('dx', 15)
+//     .attr('dy', 4)
+//
+//   textElements = textEnter.merge(textElements)
+// }
+//
+// const updateSimulation = () => {
+//   updateGraph()
+//
+//   simulation.nodes(graph.nodes).on('tick', () => {
+//     nodeElements
+//       .attr('cx', function(node) {
+//         return node.x
+//       })
+//       .attr('cy', function(node) {
+//         return node.y
+//       })
+//     textElements
+//       .attr('x', function(node) {
+//         return node.x
+//       })
+//       .attr('y', function(node) {
+//         return node.y
+//       })
+//     linkElements
+//       .attr('x1', function(link) {
+//         return link.source.x
+//       })
+//       .attr('y1', function(link) {
+//         return link.source.y
+//       })
+//       .attr('x2', function(link) {
+//         return link.target.x
+//       })
+//       .attr('y2', function(link) {
+//         return link.target.y
+//       })
+//   })
+//
+//   simulation.force('link').links(graph.links)
+//   simulation.restart()
+// }
+//
+// simulation.force('link').links(graph.links)
+//
+// updateSimulation()
