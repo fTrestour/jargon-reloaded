3fc707f80688b9c53c37ba74e9b32c68d5363da2
Unknown <ftrestour@gmail.com>
2018-05-11 15:58:13 +0200

    Adding description

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ import * as d3 from 'd3'
 import entries from './entries.json'
 import Graph from './graph'
 
+const setDescription = ({ name, description, url }) => {
+  document.querySelector(
+    '.description'
+  ).innerHTML = `<h3><a target="_blank" href=${url}>${name}</a></h3><br/>${description}`
+}
+
 const graph = new Graph({
   data: entries,
   element: document.querySelector('.chart-container'),
   mainColor: '#2e2f74',
   secondaryColor: '#52e26a',
   linkStrength: -4000,
-  onSelect: console.log
+  onSelect: setDescription
 })
